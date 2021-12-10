596e71cb130a99b26503a38e7ef5a1989ca6de9b
nialov <nikolasovaskainen@hotmail.com>
2021-12-10 22:48:27 +0200

    fix: fix fish_indent command
    The used --check is not a flag with fish 3.1.2 and
    it is not required for the check.

--- pre-commit-hooks/fish_indent_lint.fish
+++ pre-commit-hooks/fish_indent_lint.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 for f in $argv
-    if ! fish_indent --check $f
+    if ! fish_indent $f
         echo "Indentation error for $f: see diff below"
         diff -u $f (fish_indent $f | psub)
         exit 1
